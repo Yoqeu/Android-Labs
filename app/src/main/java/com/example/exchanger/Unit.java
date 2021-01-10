d3040363e19557eff61fd90157c80eff9ad660b9
Yoqeu <47595796+Yoqeu@users.noreply.github.com>
2021-01-10 18:01:22 +0300

    Added lab1

---
+++ app/src/main/java/com/example/exchanger/Unit.java
@@ -0,0 +1,13 @@
+package com.example.exchanger;
+
+public class Unit {
+    String name;
+    double coefficient;
+
+    public Unit(String name, double coefficient) {
+        this.name = name;
+        this.coefficient = coefficient;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/exchanger/Unit_t.java
@@ -0,0 +1,11 @@
+package com.example.exchanger;
+
+public class Unit_t {
+    String name;
+    double diff;
+
+    public Unit_t(String name, double diff) {
+        this.name = name;
+        this.diff = diff;
+    }
+}
